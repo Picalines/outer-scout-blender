54c9e8f006c376fd9421e897ddfd9345a2fbffbb
Picalines <picalines0@gmail.com>
2023-02-23 23:47:11 +0300

    references debug subpanel

--- panels/reference_props.py
+++ panels/reference_props.py
@@ -16,13 +16,39 @@ class OW_RECORDER_PT_reference_props(Panel):
     def draw(self, context):
         reference_props = OWRecorderReferencePropertis.from_context(context)
 
-        self.layout.prop(reference_props, 'ground_body')
-        self.layout.prop(reference_props, 'hdri_pivot')
+        self.layout.prop(reference_props, "hdri_pivot", text="")
 
-        row = self.layout.row()
-        row.enabled = not reference_props.hdri_pivot
-        row.operator(
+        column = self.layout.column()
+        column.enabled = not reference_props.hdri_pivot
+        column.operator(
             operator=OW_RECORDER_OT_create_ow_pivots.bl_idname,
             icon="OUTLINER_OB_EMPTY",
             text="Create pivots",
         )
+
+
+@bpy_register
+class OW_RECORDER_PT_reference_props_debug(Panel):
+    bl_idname = "OW_RECORDER_PT_reference_props_debug"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Outer Wilds Recorder"
+    bl_parent_id = "OW_RECORDER_PT_reference_props"
+    bl_label = "Debug"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        reference_props = OWRecorderReferencePropertis.from_context(context)
+
+        column = self.layout.column()
+        column.enabled = False
+
+        for prop_name in (
+            "ground_body",
+            "background_movie_clip",
+            "hdri_image",
+            "depth_movie_clip",
+            "hdri_node_tree",
+            "compositor_node_tree",
+        ):
+            column.prop(reference_props, prop_name, text="")
