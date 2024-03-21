ea33b19b71513107f124b8184bd2b5dc0f03dce2
Picalines <picalines0@gmail.com>
2024-03-21 16:47:41 +0300

    refactor: try to use axis_conversion

--- api/models/transform.py
+++ api/models/transform.py
@@ -1,28 +1,37 @@
 from dataclasses import dataclass
-from math import radians
 from typing import TypedDict
 
+from bpy_extras.io_utils import axis_conversion
 from mathutils import Matrix, Quaternion, Vector
 
-
-def unity_vector_to_blender(unity_vector: Vector) -> Vector:
-    x, y, z = unity_vector.xyz
-    return Vector((z, y, x))
+UNITY_TO_BLENDER = axis_conversion(from_forward="-Z", from_up="Y", to_forward="Y", to_up="Z")
+BLENDER_TO_UNITY = axis_conversion(from_forward="Y", from_up="Z", to_forward="-Z", to_up="Y")
 
 
-def unity_quaternion_to_blender(unity_quaternion: Quaternion) -> Quaternion:
-    x, y, z, w = unity_quaternion
-    return Quaternion((-w, z, y, x)) @ Quaternion((0, 1, 0), radians(90))
+def unity_transform_to_blender(unity_matrix: Matrix) -> Matrix:
+    return UNITY_TO_BLENDER.to_4x4() @ unity_matrix
 
 
-def blender_vector_to_unity(blender_vector: Vector) -> Vector:
-    x, y, z = blender_vector.xyz
-    return Vector((z, y, x))
+def blender_transform_to_unity(blender_matrix: Matrix) -> Matrix:
+    return BLENDER_TO_UNITY.to_4x4() @ blender_matrix
 
 
-def blender_quaternion_to_unity(blender_quaternion: Quaternion) -> Quaternion:
-    w, x, y, z = blender_quaternion
-    return Quaternion((z, w, x, y)) @ Quaternion((0, 1, 0), radians(-90))
+def unity_vector_to_blender(unity_vector: Vector | tuple[float, float, float]) -> Vector:
+    return UNITY_TO_BLENDER @ Vector(unity_vector)
+
+
+def unity_quaternion_to_blender(unity_quaternion: Quaternion | tuple[float, float, float, float]) -> Quaternion:
+    w, x, y, z = unity_quaternion
+    return UNITY_TO_BLENDER.to_quaternion() @ Quaternion((w, x, y, z))
+
+
+def blender_vector_to_unity(blender_vector: Vector | tuple[float, float, float]) -> Vector:
+    return BLENDER_TO_UNITY @ Vector(blender_vector)
+
+
+def blender_quaternion_to_unity(blender_quaternion: Quaternion | tuple[float, float, float, float]) -> Quaternion:
+    x, y, z, w = BLENDER_TO_UNITY.to_quaternion() @ blender_quaternion
+    return Quaternion((x, y, z, w))
 
 
 class TransformJson(TypedDict, total=False):
