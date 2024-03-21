5002d4eaec96d85711a7e53db7edeecf191c0784
Picalines <picalines0@gmail.com>
2024-03-21 16:48:45 +0300

    refactor: export only refactored operators

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,15 +1,2 @@
-from .check_api_status import *
-from .create_ow_pivots import *
-from .generate_compositor_nodes import *
-from .generate_ground_body import *
-from .generate_ground_body_background import *
-from .generate_world_nodes import *
-from .load_camera_background import *
-from .load_ground_body import *
-from .move_ground_to_origin import *
-from .record import *
-from .save_warp_transform import *
-from .set_ground_body_visible import *
-from .synchronize import *
-from .warp import *
-
+from .set_scene_origin import *
+from .warp_player import *
