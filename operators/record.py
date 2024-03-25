0411cf59859d9d240f85ba489cd49a20f522759f
Picalines <picalines0@gmail.com>
2024-03-25 15:29:48 +0300

    RecordOperator: remove obsolete properties

--- operators/record.py
+++ operators/record.py
@@ -1,87 +1,57 @@
-from os import makedirs
-
 import bpy
 from bpy.types import Context, Event, Object
 
-from ..api import APIClient
-from ..api.models import RecorderSettingsDTO, TransformDTO, get_camera_dto
+from ..api import APIClient, PostRecordingJson, PostSceneJson, Transform
 from ..bpy_register import bpy_register
-from ..preferences import OWRecorderPreferences
-from ..properties import OWRecorderProperties, OWRecorderReferenceProperties, OWRecorderSceneProperties
-from ..utils import GeneratorWithState, get_footage_path, iter_with_prev
+from ..properties import RecordingProperties, SceneProperties
+from ..utils import GeneratorWithState
 from .async_operator import AsyncOperator
 
 
 @bpy_register
-class OW_RECORDER_OT_record(AsyncOperator):
-    """Record footage from Outer Wilds and import it to current project (save project before running)"""
+class RecordOperator(AsyncOperator):
+    """Record footage from Outer Wilds and import it to current project"""
 
-    bl_idname = "ow_recorder.record"
+    bl_idname = "outer_scout.record"
     bl_label = "Record"
-
-    _timer = None
 
     @classmethod
     def poll(cls, context) -> bool:
-        reference_props = OWRecorderReferenceProperties.from_context(context)
-        return all(
-            (
-                bpy.data.is_saved,
-                context.scene.camera,
-                reference_props.ground_body,
-                reference_props.hdri_pivot,
-            )
-        )
+        if not bpy.data.is_saved:
+            cls.poll_message_set("Save project before recording")
+            return False
+
+        scene_props = SceneProperties.from_context(context)
+        return scene_props.is_scene_created
 
     def _run_async(self, context: Context):
-        api_client = APIClient(OWRecorderPreferences.from_context(context))
+        api_client = APIClient.from_context(context)
 
-        recorder_status = api_client.get_recorder_status()
+        recording_status = api_client.get_recording_status()
 
-        if recorder_status["enabled"]:
-            self.report({"ERROR"}, "already recording")
+        if recording_status["inProgress"]:
+            self.report({"ERROR"}, "recording is in progress")
             return {"CANCELLED"}
 
         scene = context.scene
-
         was_on_frame = scene.frame_current
-
-        recorder_props = OWRecorderProperties.from_context(context)
-        scene_props = OWRecorderSceneProperties.from_context(context)
-        ref_props = OWRecorderReferenceProperties.from_context(context)
-
+        recording_props = RecordingProperties.from_context(context)
+        scene_props = SceneProperties.from_context(context)
         frame_count = scene.frame_end - scene.frame_start + 1
 
-        footage_path = get_footage_path(context)
-        makedirs(footage_path, exist_ok=True)
-
-        recorder_settings: RecorderSettingsDTO = {
-            "outputDirectory": footage_path,
-            "startFrame": scene.frame_start,
-            "endFrame": scene.frame_end,
-            "frameRate": scene.render.fps,
-            "resolutionX": scene.render.resolution_x,
-            "resolutionY": scene.render.resolution_y,
-            "recordHdri": recorder_props.record_hdri,
-            "recordDepth": recorder_props.record_depth,
-            "hdriFaceSize": recorder_props.hdri_face_size,
-            "hidePlayerModel": recorder_props.hide_player_model,
+        scene_json: PostSceneJson = {
+            "origin": Transform.from_matrix(scene_props.origin_matrix).to_json(parent=scene_props.origin_parent),
+            "hidePlayerModel": scene_props.hide_player_model,
         }
 
-        if not api_client.set_recorder_settings(recorder_settings):
-            self.report({"ERROR"}, "failed to set recorder settings")
+        if scene_creation_error := api_client.post_scene(scene_json):
+            self.report({"ERROR"}, scene_creation_error["error"])
             return {"CANCELLED"}
 
-        recorder_status = api_client.get_recorder_status()
+        self._add_timer(context, recording_props.modal_timer_delay)
 
-        if not recorder_status["isAbleToRecord"]:
-            self.report({"ERROR"}, "unable to record")
-            return {"CANCELLED"}
-
-        self._timer = context.window_manager.event_timer_add(recorder_props.modal_timer_delay, window=context.window)
-
-        recorder_props.stage_progress = 0
-        recorder_props.is_recording = True
+        recording_props.stage_progress = 0
+        recording_props.is_recording = True
 
         # send animation data
 
@@ -89,15 +59,15 @@ class OW_RECORDER_OT_record(AsyncOperator):
         hdri_pivot: Object = ref_props.hdri_pivot
         camera = scene.camera
 
-        recorder_props.stage_description = "Sending animation"
+        recording_props.stage_description = "Sending animation"
 
-        max_chunk_size = recorder_props.animation_chunk_size
+        max_chunk_size = recording_props.animation_chunk_size
 
         chunk_keyframe_values: dict[str, list] = {
             prop_name: [None] * max_chunk_size
             for prop_name in (
                 "free-camera/transform",
-                *(("hdri-pivot/transform",) if recorder_props.record_hdri else ()),
+                *(("hdri-pivot/transform",) if recording_props.record_hdri else ()),
                 "free-camera/camera-info",
                 "time/scale",
             )
@@ -105,14 +75,14 @@ class OW_RECORDER_OT_record(AsyncOperator):
 
         animated_transform_objects = {
             "free-camera/transform": camera,
-            **({"hdri-pivot/transform": hdri_pivot} if recorder_props.record_hdri else {}),
+            **({"hdri-pivot/transform": hdri_pivot} if recording_props.record_hdri else {}),
         }
 
         scene.frame_set(frame=scene.frame_start)
 
         while True:
             frame_number = scene.frame_current - scene.frame_start + 1
-            recorder_props.stage_progress = frame_number / frame_count
+            recording_props.stage_progress = frame_number / frame_count
 
             current_chunk_size = 0
             chunk_start_frame = scene.frame_current
@@ -124,7 +94,7 @@ class OW_RECORDER_OT_record(AsyncOperator):
 
                 for prop_api_name, transform_object in animated_transform_objects.items():
                     local_matrix = ground_body.matrix_world.inverted() @ transform_object.matrix_world
-                    local_transform = TransformDTO.from_matrix(local_matrix)
+                    local_transform = Transform.from_matrix(local_matrix)
                     transform_keyframe = local_transform.blender_to_unity().to_json()
 
                     chunk_keyframe_values[prop_api_name][chunk_frame_offset] = transform_keyframe
@@ -156,32 +126,36 @@ class OW_RECORDER_OT_record(AsyncOperator):
 
         # record
 
+        recording_json: PostRecordingJson = {
+            "startFrame": scene.frame_start,
+            "endFrame": scene.frame_end,
+            "frameRate": scene.render.fps,
+        }
+
         if not api_client.set_recorder_enabled(True):
             self.report({"ERROR"}, "failed to start recording")
             return {"CANCELLED"}
 
-        recorder_props.stage_description = "Recording"
+        recording_props.stage_description = "Recording"
 
         frames_recorded = GeneratorWithState(api_client.get_frames_recorded_async())
 
         for prev_count, current_count in iter_with_prev(frames_recorded):
-            recorder_props.stage_progress = current_count / frame_count
+            recording_props.stage_progress = current_count / frame_count
 
             if prev_count != current_count:
-                recorder_status = api_client.get_recorder_status()
-                if not recorder_status["enabled"]:
+                recording_status = api_client.get_recorder_status()
+                if not recording_status["enabled"]:
                     break
 
             yield {"TIMER"}
 
         if frames_recorded.returned == False:
-            self._remove_timer(context)
             self.report({"ERROR"}, "failed to receive recorded frames count")
             return {"CANCELLED"}
 
         # end
 
-        self._remove_timer(context)
         self.report({"INFO"}, "Outer Wilds render finished")
 
         return {"FINISHED"}
@@ -189,13 +163,7 @@ class OW_RECORDER_OT_record(AsyncOperator):
     def _after_event(self, context: Context, _: Event):
         context.area.tag_redraw()
 
-    def _remove_timer(self, context: Context):
-        recorder_props = OWRecorderProperties.from_context(context)
-
-        context.window_manager.event_timer_remove(self._timer)
-        recorder_props.is_recording = False
-
-    def _get_transform_local_to(self, parent: Object, child: Object) -> TransformDTO:
+    def _get_transform_local_to(self, parent: Object, child: Object) -> Transform:
         local_matrix = parent.matrix_world.inverted() @ child.matrix_world
-        return TransformDTO.from_matrix(local_matrix)
+        return Transform.from_matrix(local_matrix)
 
