f4efef7204dba22cecc1896a0e54ccbccc55d0b5
Picalines <picalines0@gmail.com>
2024-03-29 03:04:29 +0300

    operators: record operator sends camera animation

--- operators/record.py
+++ operators/record.py
@@ -2,25 +2,14 @@ from math import radians
 
 import bpy
 from bpy.path import abspath, clean_name
-from bpy.types import Camera, Context, Event, Object
+from bpy.types import ID, Camera, Context, CopyTransformsConstraint, Event, FCurve, Object
 from mathutils import Matrix
 
-from ..api import APIClient, Transform
+from ..api import LEFT_HANDED_TO_RIGHT, RIGHT_HANDED_TO_LEFT, APIClient, Transform
 from ..bpy_register import bpy_register
 from ..properties import CameraProperties, RecordingProperties, SceneProperties
 from ..utils import Result, operator_do
 from .async_operator import AsyncOperator
-
-
-def get_camera_gate_fit(context: Context, camera: Camera):
-    match camera.sensor_fit:
-        case "AUTO":
-            render = context.scene.render
-            return "horizontal" if render.resolution_x > render.resolution_y else "vertical"
-        case "VERTICAL":
-            return "vertical"
-        case "HORIZONTAL":
-            return "horizontal"
 
 
 @bpy_register
@@ -59,11 +48,7 @@ class RecordOperator(AsyncOperator):
 
         self._create_cameras(context, api_client).then()
 
-        # send animation data
-
-        # self._send_keyframes(context, api_client).then()
-
-        # record
+        self._send_camera_keyframes(context, api_client).then()
 
         api_client.post_scene_recording(
             {
@@ -100,12 +85,14 @@ class RecordOperator(AsyncOperator):
             (camera_obj, camera_obj.data) for camera_obj in scene.objects if camera_obj.type == "CAMERA"
         ]
 
+        scene.frame_set(scene.frame_start)
+
         for object, camera in cameras:
             camera_props = CameraProperties.of_camera(camera)
             if camera_props.outer_scout_type == "NONE":
                 continue
 
-            object_name = clean_name(camera.name)
+            object_name = get_camera_api_name(camera)
 
             camera_transform = Transform.from_matrix(object.matrix_world @ Matrix.Rotation(radians(-90), 4, "X"))
 
@@ -160,79 +147,112 @@ class RecordOperator(AsyncOperator):
                 ).then()
 
     @Result.do()
-    def _send_keyframes(self, context: Context, api_client: APIClient):
+    def _send_camera_keyframes(self, context: Context, api_client: APIClient):
         scene = context.scene
-        was_on_frame = scene.frame_current
-        frame_count = scene.frame_end - scene.frame_start + 1
+        scene_frame_range = range(scene.frame_start, scene.frame_end + 1)
 
-        ground_body: Object = ref_props.ground_body
-        hdri_pivot: Object = ref_props.hdri_pivot
-        camera = scene.camera
+        cameras: list[tuple[Object, Camera]] = [
+            (camera_obj, camera_obj.data) for camera_obj in scene.objects if camera_obj.type == "CAMERA"
+        ]
 
-        recording_props.stage_description = "Sending animation"
+        vector_index_to_axis = {0: "x", 1: "y", 2: "z", 3: "w"}
+        quaternion_index_to_axis = {0: "w", 1: "x", 2: "y", 3: "z"}
 
-        max_chunk_size = recording_props.animation_chunk_size
-
-        chunk_keyframe_values: dict[str, list] = {
-            prop_name: [None] * max_chunk_size
-            for prop_name in (
-                "free-camera/transform",
-                *(("hdri-pivot/transform",) if recording_props.record_hdri else ()),
-                "free-camera/camera-info",
-                "time/scale",
-            )
+        camera_props_to_track = {
+            "PERSPECTIVE": {
+                "camera.perspective.focalLength": "lens",
+                "camera.perspective.sensorSize.x": "sensor_width",
+                "camera.perspective.sensorSize.y": "sensor_height",
+                "camera.perspective.lensShift.x": "shift_x",
+                "camera.perspective.lensShift.y": "shift_y",
+                "camera.perspective.nearClipPlane": "clip_start",
+                "camera.perspective.farClipPlane": "clip_end",
+            },
+            "EQUIRECTANGULAR": {},
         }
 
-        animated_transform_objects = {
-            "free-camera/transform": camera,
-            **({"hdri-pivot/transform": hdri_pivot} if recording_props.record_hdri else {}),
-        }
+        bpy.ops.object.empty_add()
+        left_to_right_empty: Object = context.active_object
+        left_to_right_empty.matrix_world = LEFT_HANDED_TO_RIGHT
+        left_to_right_empty.name = "outer_scout.left_to_right_empty"
 
-        scene.frame_set(frame=scene.frame_start)
+        bpy.ops.object.empty_add()
+        camera_rotation_empty: Object = context.active_object
+        camera_rotation_empty.matrix_world = Matrix.Rotation(radians(-90), 4, "X")
+        camera_rotation_empty.name = "outer_scout.camera_rotation"
 
-        while True:
-            frame_number = scene.frame_current - scene.frame_start + 1
-            recording_props.stage_progress = frame_number / frame_count
+        try:
+            for camera_object, camera in cameras:
+                camera_props = CameraProperties.of_camera(camera)
+                if camera_props.outer_scout_type == "NONE":
+                    continue
 
-            current_chunk_size = 0
-            chunk_start_frame = scene.frame_current
-            at_end = False
+                object_name = get_camera_api_name(camera)
 
-            for chunk_frame_offset in range(max_chunk_size):
-                scene.frame_set(frame=chunk_start_frame + chunk_frame_offset)
-                current_chunk_size += 1
+                bpy.ops.object.select_all(action="DESELECT")
+                bpy.ops.object.empty_add()
+                camera_track_empty: Object = context.active_object
+                camera_track_empty.name = f"outer_scout.{object_name}.track"
+                camera_track_empty.rotation_mode = "QUATERNION"
 
-                for prop_api_name, transform_object in animated_transform_objects.items():
-                    local_matrix = ground_body.matrix_world.inverted() @ transform_object.matrix_world
-                    local_transform = Transform.from_matrix(local_matrix)
-                    transform_keyframe = local_transform.blender_to_unity().to_json()
+                # idea is to implement right_matrix_to_left using the Blender constraints
+                # and also add X rotation to the camera
+                camera_track_empty.matrix_world = RIGHT_HANDED_TO_LEFT
+                add_copy_transform_constraint(camera_track_empty, camera_object, mix_mode="AFTER_FULL")
+                add_copy_transform_constraint(camera_track_empty, camera_rotation_empty, mix_mode="AFTER_FULL")
+                add_copy_transform_constraint(camera_track_empty, left_to_right_empty, mix_mode="AFTER_FULL")
 
-                    chunk_keyframe_values[prop_api_name][chunk_frame_offset] = transform_keyframe
-
-                chunk_keyframe_values["free-camera/camera-info"][chunk_frame_offset] = get_camera_dto(camera.data)
-
-                chunk_keyframe_values["time/scale"][chunk_frame_offset] = scene_props.time_scale
-
-                if scene.frame_current >= scene.frame_end:
-                    at_end = True
-                    break
-
-            for prop_api_name, keyframe_values in chunk_keyframe_values.items():
-                keyframe_values = keyframe_values[:current_chunk_size]
-                success = api_client.set_keyframes(prop_api_name, chunk_start_frame, keyframe_values)
-
-                if not success:
-                    self.report(
-                        {"WARNING"},
-                        f"failed to send animation {prop_api_name} data at frame {chunk_start_frame}",
+                for camera_prop, camera_prop_path in camera_props_to_track[camera_props.outer_scout_type].items():
+                    camera_track_empty[camera_prop] = 0.0
+                    add_single_prop_copy_driver(
+                        camera_track_empty,
+                        f'["{camera_prop}"]',
+                        target_id_type="CAMERA",
+                        target_id=camera,
+                        target_data_path=camera_prop_path,
                     )
 
-            if at_end:
-                break
+                bpy.ops.nla.bake(
+                    frame_start=scene.frame_start,
+                    frame_end=scene.frame_end,
+                    step=1,
+                    only_selected=True,
+                    visual_keying=True,
+                    clear_constraints=True,
+                    clean_curves=True,
+                    bake_types={"OBJECT"},
+                    channel_types={"LOCATION", "ROTATION", "SCALE", "PROPS"},
+                )
 
-            yield {"TIMER"}
+                track_action = camera_track_empty.animation_data.action
 
-        scene.frame_set(frame=was_on_frame)
+                animation_properties_json = {}
+
+                for fcurve in track_action.fcurves:
+                    fcurve: FCurve
+
+                    match fcurve.data_path:
+                        case "location":
+                            outer_scout_property = "transform.position." + vector_index_to_axis[fcurve.array_index]
+                        case "rotation_quaternion":
+                            outer_scout_property = "transform.rotation." + quaternion_index_to_axis[fcurve.array_index]
+                        case "scale":
+                            outer_scout_property = "transform.scale." + vector_index_to_axis[fcurve.array_index]
+                        case custom_data_path:
+                            outer_scout_property = custom_data_path[2:-2]
+
+                    animation_properties_json[outer_scout_property] = {
+                        "keyframes": {frame: {"value": fcurve.evaluate(frame)} for frame in scene_frame_range}
+                    }
+
+                try:
+                    api_client.put_keyframes(object_name, {"properties": animation_properties_json}).then()
+                finally:
+                    bpy.data.actions.remove(track_action, do_unlink=True)
+                    bpy.data.objects.remove(camera_track_empty, do_unlink=True)
+        finally:
+            bpy.data.objects.remove(left_to_right_empty, do_unlink=True)
+            bpy.data.objects.remove(camera_rotation_empty, do_unlink=True)
 
     def _after_event(self, context: Context, _: Event):
         context.area.tag_redraw()
@@ -240,4 +260,51 @@ class RecordOperator(AsyncOperator):
     def _get_transform_local_to(self, parent: Object, child: Object) -> Transform:
         local_matrix = parent.matrix_world.inverted() @ child.matrix_world
         return Transform.from_matrix(local_matrix)
+
+
+def get_camera_api_name(camera: Camera):
+    return clean_name(camera.name)
+
+
+def get_camera_gate_fit(context: Context, camera: Camera):
+    match camera.sensor_fit:
+        case "AUTO":
+            render = context.scene.render
+            return "horizontal" if render.resolution_x > render.resolution_y else "vertical"
+        case "VERTICAL":
+            return "vertical"
+        case "HORIZONTAL":
+            return "horizontal"
+
+
+def add_copy_transform_constraint(object: Object, target: Object, *, mix_mode: str):
+    constraint: CopyTransformsConstraint = object.constraints.new("COPY_TRANSFORMS")
+
+    constraint.target = target
+    constraint.mix_mode = mix_mode
+
+    return constraint
+
+
+def add_single_prop_copy_driver(
+    object: Object,
+    data_path: str,
+    *,
+    target_id_type: str,
+    target_id: ID,
+    target_data_path: str,
+    array_index=-1,
+    var_name="v",
+):
+    driver = object.driver_add(data_path, array_index).driver
+
+    driver_var = driver.variables.new()
+    driver_var.name = var_name
+    driver_var.type = "SINGLE_PROP"
+    driver_var.targets[0].id_type = target_id_type
+    driver_var.targets[0].id = target_id
+    driver_var.targets[0].data_path = target_data_path
+    driver.expression = driver_var.name
+
+    return driver
 
