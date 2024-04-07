16594a01c8d163e9e756335753c5b39ac2b7cee1
Picalines <picalines0@gmail.com>
2024-04-08 00:26:23 +0300

    operators: refactor record to single keyframes method, add transform recorders

--- operators/record.py
+++ operators/record.py
@@ -4,16 +4,17 @@ from operator import delitem
 from typing import Callable
 
 import bpy
-from bpy.path import clean_name
 from bpy.types import Camera, Context, Event, FCurve, Object
 from bpy_extras.anim_utils import BakeOptions, bake_action
 from mathutils import Matrix
 
 from ..api import LEFT_HANDED_TO_RIGHT, RIGHT_HANDED_TO_LEFT, APIClient, Transform
 from ..bpy_register import bpy_register
-from ..properties import CameraProperties, RecordingProperties, SceneProperties
+from ..properties import CameraProperties, ObjectProperties, SceneProperties, SceneRecordingProperties
 from ..utils import Result, add_driver, defer, operator_do, with_defers
 from .async_operator import AsyncOperator
+
+ORIGIN_OBJECT_NAME = "__scene_origin"
 
 
 @bpy_register
@@ -35,7 +36,7 @@ class RecordOperator(AsyncOperator):
     @operator_do
     @with_defers
     def _run_async(self, context):
-        recording_props = RecordingProperties.from_context(context)
+        recording_props = SceneRecordingProperties.from_context(context)
 
         if recording_props.in_progress:
             Result.do_error("recording is in progress")
@@ -44,12 +45,7 @@ class RecordOperator(AsyncOperator):
         api_client = APIClient.from_context(context)
         scene = context.scene
 
-        api_client.post_scene(
-            {
-                "origin": Transform.from_matrix(scene_props.origin_matrix).to_json(parent=scene_props.origin_parent),
-                "hidePlayerModel": scene_props.hide_player_model,
-            }
-        ).then()
+        api_client.post_scene({"hidePlayerModel": scene_props.hide_player_model}).then()
 
         defer(api_client.delete_scene)
 
@@ -58,9 +54,7 @@ class RecordOperator(AsyncOperator):
 
         self._create_objects(context, api_client).then()
 
-        self._send_scene_keyframes(context, api_client).then()
-
-        self._send_camera_keyframes(context, api_client).then()
+        self._send_keyframes(context, api_client).then()
 
         api_client.post_scene_recording(
             {
@@ -92,35 +86,53 @@ class RecordOperator(AsyncOperator):
     @Result.do()
     def _create_objects(self, context: Context, api_client: APIClient):
         scene = context.scene
+        scene_props = SceneProperties.from_context(context)
 
         scene.frame_set(scene.frame_start)
 
+        api_client.post_object(
+            name=ORIGIN_OBJECT_NAME,
+            transform=Transform.from_matrix(scene_props.origin_matrix),
+            parent=scene_props.origin_parent,
+        ).then()
+
         for object in scene.objects:
             object: Object
+            object_props = ObjectProperties.of_object(object)
+
+            if not object_props.has_unity_object_name:
+                continue
+
+            if object.type == "CAMERA" or object_props.object_type == "CUSTOM":
+                object_matrix = object.matrix_world
+                if object.type == "CAMERA":
+                    object_matrix @= Matrix.Rotation(radians(-90), 4, "X")
+
+                api_client.post_object(
+                    name=object_props.unity_object_name,
+                    transform=Transform.from_matrix(object_matrix).to_left(),
+                    parent=ORIGIN_OBJECT_NAME,
+                ).then()
 
             if object.type == "CAMERA":
-                self._create_camera(context, api_client, object).then()
+                self._add_camera(context, api_client, object, object_props.unity_object_name).then()
+            else:
+                self._add_transform_recorder(api_client, object, object_props.unity_object_name).then()
 
     @Result.do()
-    def _create_camera(self, context: Context, api_client: APIClient, object: Object):
+    def _add_camera(self, context: Context, api_client: APIClient, object: Object, object_api_name: str):
         camera: Camera = object.data
         camera_props = CameraProperties.of_camera(camera)
-        if not camera_props.is_used_in_scene:
+        if not camera_props.is_active:
             return
 
         scene = context.scene
-
-        object_name = get_camera_api_name(camera)
-
-        camera_transform = Transform.from_matrix(object.matrix_world @ Matrix.Rotation(radians(-90), 4, "X"))
-
-        api_client.post_object(name=object_name, transform=camera_transform.to_left()).then()
 
         match camera_props.outer_scout_type:
             case "PERSPECTIVE":
                 camera.lens_unit = "MILLIMETERS"
                 api_client.post_perspective_camera(
-                    object_name,
+                    object_api_name,
                     {
                         "gateFit": get_camera_gate_fit(context, camera),
                         "resolution": {"width": scene.render.resolution_x, "height": scene.render.resolution_y},
@@ -135,100 +147,43 @@ class RecordOperator(AsyncOperator):
                 ).then()
 
             case "EQUIRECTANGULAR":
-                api_client.post_equirect_camera(object_name, {"faceResolution": camera_props.equirect_face_size}).then()
+                api_client.post_equirect_camera(
+                    object_api_name, {"faceResolution": camera_props.equirect_face_size}
+                ).then()
 
             case not_implemented_camera_type:
                 raise NotImplementedError(f"camera of type {not_implemented_camera_type} is not implemented")
 
         if camera_props.color_texture_props.has_recording_path:
-            api_client.post_texture_recorder(object_name, "color", camera_props.color_texture_props).then()
+            api_client.post_texture_recorder(object_api_name, "color", camera_props.color_texture_props).then()
 
         if camera_props.depth_texture_props.has_recording_path:
-            api_client.post_texture_recorder(object_name, "depth", camera_props.depth_texture_props).then()
+            api_client.post_texture_recorder(object_api_name, "depth", camera_props.depth_texture_props).then()
+
+    @Result.do()
+    def _add_transform_recorder(self, api_client: APIClient, object: Object, object_api_name: str):
+        object_props = ObjectProperties.of_object(object)
+        transform_props = object_props.transform_props
+
+        if not transform_props.has_recording_path or transform_props.mode != "RECORD":
+            return
+
+        api_client.post_transform_recorder(
+            object_api_name,
+            {"format": "json", "outputPath": transform_props.absolute_recording_path, "origin": ORIGIN_OBJECT_NAME},
+        ).then()
 
     @Result.do()
     @with_defers
-    def _send_scene_keyframes(self, context: Context, api_client: APIClient):
+    def _send_keyframes(self, context: Context, api_client: APIClient):
         scene = context.scene
-        scene_frame_range = range(scene.frame_start, scene.frame_end + 1)
         scene_props = SceneProperties.from_context(context)
+        scene_frame_range = range(scene.frame_start, scene.frame_end + 1)
 
-        left_to_right_empty = add_temp_empty(context, "outer_scout.left_to_right", matrix_world=LEFT_HANDED_TO_RIGHT)
-
-        ground_inverse_empty = add_temp_empty(context, "outer_scout.ground_inverse")
-        generate_inverted_transform_drivers(ground_inverse_empty, scene_props.ground_body)
-
-        origin_empty = add_temp_empty(context, "outer_scout.scene.origin", matrix_world=RIGHT_HANDED_TO_LEFT)
-        add_multiply_transform_constraint(origin_empty, ground_inverse_empty)
-        add_multiply_transform_constraint(origin_empty, left_to_right_empty)
+        vector_index_to_axis = {0: "x", 1: "y", 2: "z", 3: "w"}
+        quaternion_index_to_axis = {0: "w", 1: "x", 2: "y", 3: "z"}  # order is converted for Unity
 
         scene_props_to_track = {"time.scale": "outer_scout_scene.time_scale"}
-
-        for scene_prop, scene_prop_path in scene_props_to_track.items():
-            origin_empty[scene_prop] = 0.0
-            add_driver(origin_empty, f'["{scene_prop}"]', "value", value=(scene, scene_prop_path))
-
-        track_action = bake_action(
-            origin_empty,
-            action=None,
-            frames=scene_frame_range,
-            bake_options=BakeOptions(
-                only_selected=True,
-                do_pose=False,
-                do_object=True,
-                do_visual_keying=True,
-                do_constraint_clear=True,
-                do_parents_clear=True,
-                do_clean=True,
-                do_location=True,
-                do_rotation=True,
-                do_scale=False,
-                do_bbone=False,
-                do_custom_props=True,
-            ),
-        )
-
-        defer(bpy.data.actions.remove, track_action, do_unlink=True)
-
-        vector_index_to_axis = {0: "x", 1: "y", 2: "z", 3: "w"}
-        quaternion_index_to_axis = {0: "w", 1: "x", 2: "y", 3: "z"}
-
-        scene_animation_properties_json = {}
-        origin_animation_properties_json = {}
-
-        for fcurve in track_action.fcurves:
-            fcurve: FCurve
-
-            match fcurve.data_path:
-                case "location":
-                    outer_scout_property = "transform.position." + vector_index_to_axis[fcurve.array_index]
-                    animation_properties_json = origin_animation_properties_json
-                case "rotation_quaternion":
-                    outer_scout_property = "transform.rotation." + quaternion_index_to_axis[fcurve.array_index]
-                    animation_properties_json = origin_animation_properties_json
-                case custom_data_path:
-                    outer_scout_property = custom_data_path[2:-2]
-                    animation_properties_json = scene_animation_properties_json
-
-            animation_properties_json[outer_scout_property] = {
-                "keyframes": {frame: {"value": fcurve.evaluate(frame)} for frame in scene_frame_range}
-            }
-
-        api_client.put_scene_keyframes({"properties": scene_animation_properties_json}).then()
-        api_client.put_object_keyframes("scene.origin", {"properties": origin_animation_properties_json}).then()
-
-    @Result.do()
-    @with_defers
-    def _send_camera_keyframes(self, context: Context, api_client: APIClient):
-        scene = context.scene
-        scene_frame_range = range(scene.frame_start, scene.frame_end + 1)
-
-        cameras: list[tuple[Object, Camera]] = [
-            (camera_obj, camera_obj.data) for camera_obj in scene.objects if camera_obj.type == "CAMERA"
-        ]
-
-        vector_index_to_axis = {0: "x", 1: "y", 2: "z", 3: "w"}
-        quaternion_index_to_axis = {0: "w", 1: "x", 2: "y", 3: "z"}
 
         camera_props_to_track = {
             "PERSPECTIVE": {
@@ -244,34 +199,61 @@ class RecordOperator(AsyncOperator):
 
         left_to_right_empty = add_temp_empty(context, "outer_scout.left_to_right", matrix_world=LEFT_HANDED_TO_RIGHT)
 
+        ground_inverse_empty = add_temp_empty(context, "outer_scout.ground_inverse")
+        if scene_props.has_ground_body:
+            generate_inverted_transform_drivers(ground_inverse_empty, scene_props.ground_body)
+        else:
+            ground_inverse_empty.matrix_world = Matrix.Identity(4)
+
         camera_rotation_empty = add_temp_empty(
             context, "outer_scout.camera_rotation", matrix_world=Matrix.Rotation(radians(-90), 4, "X")
         )
 
-        for camera_object, camera in cameras:
-            camera_props = CameraProperties.of_camera(camera)
-            if not camera_props.is_used_in_scene:
+        for object in scene.objects:
+            object: Object
+            object_props = ObjectProperties.of_object(object)
+
+            if not (
+                object is ground_inverse_empty
+                or (
+                    object_props.has_unity_object_name
+                    and (object.type == "CAMERA" or object_props.transform_props.mode == "APPLY")
+                )
+            ):
                 continue
 
-            object_name = get_camera_api_name(camera)
-
-            camera_empty = add_temp_empty(
-                context, f"outer_scout.camera.{object_name}", matrix_world=RIGHT_HANDED_TO_LEFT
+            object_track_empty = add_temp_empty(
+                context,
+                (
+                    f"outer_scout.object.{object_props.unity_object_name}"
+                    if object_props.has_unity_object_name
+                    else "outer_scout.scene.origin"
+                ),
+                matrix_world=RIGHT_HANDED_TO_LEFT,
             )
 
-            # idea is to implement right_matrix_to_left using the Blender constraints
-            # and also add X rotation to the camera
-            add_multiply_transform_constraint(camera_empty, camera_object)
-            add_multiply_transform_constraint(camera_empty, camera_rotation_empty)
-            add_multiply_transform_constraint(camera_empty, left_to_right_empty)
+            camera_props = CameraProperties.of_camera(object.data) if object.type == "CAMERA" else None
 
-            if camera_props.outer_scout_type in camera_props_to_track:
+            if camera_props and camera_props.outer_scout_type in camera_props_to_track:
                 for camera_prop, camera_prop_path in camera_props_to_track[camera_props.outer_scout_type].items():
-                    camera_empty[camera_prop] = 0.0
-                    add_driver(camera_empty, f'["{camera_prop}"]', "value", value=(camera, camera_prop_path))
+                    object_track_empty[camera_prop] = 0.0
+                    add_driver(object_track_empty, f'["{camera_prop}"]', "value", value=(object.data, camera_prop_path))
+
+            # idea is to implement right_matrix_to_left using the Blender constraints
+            add_multiply_transform_constraint(object_track_empty, object)
+
+            if object.type == "CAMERA":
+                # in Unity camera looks forward, but in Blender it looks down
+                add_multiply_transform_constraint(object_track_empty, camera_rotation_empty)
+            elif object is ground_inverse_empty:
+                for scene_prop, scene_prop_path in scene_props_to_track.items():
+                    object_track_empty[scene_prop] = 0.0
+                    add_driver(object_track_empty, f'["{scene_prop}"]', "value", value=(scene, scene_prop_path))
+
+            add_multiply_transform_constraint(object_track_empty, left_to_right_empty)
 
             track_action = bake_action(
-                camera_empty,
+                object_track_empty,
                 action=None,
                 frames=scene_frame_range,
                 bake_options=BakeOptions(
@@ -292,7 +274,8 @@ class RecordOperator(AsyncOperator):
 
             defer(bpy.data.actions.remove, track_action, do_unlink=True)
 
-            animation_properties_json = {}
+            object_animation_properties_json = {}
+            scene_animation_properties_json = {}
 
             for fcurve in track_action.fcurves:
                 fcurve: FCurve
@@ -300,23 +283,32 @@ class RecordOperator(AsyncOperator):
                 match fcurve.data_path:
                     case "location":
                         outer_scout_property = "transform.position." + vector_index_to_axis[fcurve.array_index]
+                        animation_properties_json = object_animation_properties_json
                     case "rotation_quaternion":
                         outer_scout_property = "transform.rotation." + quaternion_index_to_axis[fcurve.array_index]
+                        animation_properties_json = object_animation_properties_json
                     case custom_data_path:
                         outer_scout_property = custom_data_path[2:-2]
+                        animation_properties_json = (
+                            scene_animation_properties_json
+                            if outer_scout_property in scene_props_to_track
+                            else object_animation_properties_json
+                        )
 
                 animation_properties_json[outer_scout_property] = {
                     "keyframes": {frame: {"value": fcurve.evaluate(frame)} for frame in scene_frame_range}
                 }
 
-            api_client.put_object_keyframes(object_name, {"properties": animation_properties_json}).then()
+            api_client.put_object_keyframes(
+                ORIGIN_OBJECT_NAME if object is ground_inverse_empty else object_props.unity_object_name,
+                {"properties": object_animation_properties_json},
+            ).then()
+
+            if len(scene_animation_properties_json):
+                api_client.put_scene_keyframes({"properties": scene_animation_properties_json}).then()
 
     def _after_event(self, context: Context, _: Event):
         context.area.tag_redraw()
-
-
-def get_camera_api_name(camera: Camera):
-    return clean_name(camera.name)
 
 
 def get_camera_gate_fit(context: Context, camera: Camera):
