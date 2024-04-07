2432ae72c8cebfdd7877a378a90021e34b07cedb
Picalines <picalines0@gmail.com>
2024-04-07 21:33:38 +0300

    properties: don't allow scene.origin name

--- properties/object_props.py
+++ properties/object_props.py
@@ -1,14 +1,32 @@
-from bpy.props import PointerProperty, StringProperty
+from bpy.props import EnumProperty, PointerProperty, StringProperty
 from bpy.types import Object, PropertyGroup
 
 from ..bpy_register import bpy_register_property
 from .transform_recording_props import TransformRecordingProperties
 
 
+def on_unity_object_name_update(object_props: "ObjectProperties", _):
+    if object_props.unity_object_name == "scene.origin":
+        object_props.unity_object_name = ""
+
+
 @bpy_register_property(Object, "outer_scout_object")
 class ObjectProperties(PropertyGroup):
     unity_object_name: StringProperty(
-        name="Unity Object Name", description="The name of the GameObject from Outer Wilds", default="", options=set()
+        name="Unity Object Name",
+        description="The name of the GameObject from Outer Wilds",
+        default="",
+        options=set(),
+        update=on_unity_object_name_update,
+    )
+
+    object_type: EnumProperty(
+        name="Unity Object Mode",
+        default="NEW",
+        items=[
+            ("NEW", "Custom", "Creates new empty GameObject in Unity"),
+            ("EXISTING", "Existing", "Uses existing Unity GameObject"),
+        ],
     )
 
     transform_recording: PointerProperty(type=TransformRecordingProperties)
